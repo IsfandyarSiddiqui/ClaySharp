5c249bb1a37a3fc2fcbd51c218a54e7abe70396e
Isfandyar Siddiqui <isfandyarsiddiqui@gmail.com>
2026-05-31 21:39:45 +0500

    bring back the reference to the clay library.

--- SharpClay/Layout/ClayUI.cs
+++ SharpClay/Layout/ClayUI.cs
@@ -6,7 +6,4 @@ public static class ClayUI
 {
     public static ClayElementScope Container(in ElementDeclaration declaration) => 
         new ClayElementScope(in declaration);
-
-    public static ClayElementScope Panel(in ElementDeclaration declaration) => 
-        new ClayElementScope(in declaration);
 }
